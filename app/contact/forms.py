4c297c3d83e6064054f281208cae85355425fd93
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-13 18:36:30 +0100

    flash rdy

--- app/contact/forms.py
+++ app/contact/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, TextAreaField,
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email
 
 ## contact form
 
 class ContactForm(FlaskForm):
 
-    name = StringField('Name:', id='name_contact', [DataRequired()])
-    email = StringField('E-Mail:', id='mail_contact', [Email(message=('Not a valid E-Mail address.')),DataRequired()])
-    body = TextAreaField('Message:', id='body_contact', [DataRequired(),Length(min=50, message=('Your message is too short.'))])
-    recaptcha = RecaptchaField(id='recaptcha_contact')
-    submit = SubmitField('Submit', id='submit_contact')
+    name = StringField('Name:',  [DataRequired()])
+    email = StringField('E-Mail:', [Email(message=('Not a valid E-Mail address.')),DataRequired()])
+    body = TextAreaField('Message:',  [DataRequired(),Length(min=4, message=('Your message is too short.'))])
+    recaptcha = RecaptchaField()
+    submit = SubmitField('Submit')
 
 
--- app/contact/routes.py
+++ app/contact/routes.py
@@ -1,8 +1,21 @@
 from . import blueprint
-from flask import render_template
+from flask import render_template,flash, redirect, url_for
 from flask_login import login_required, current_user
+from .forms import ContactForm
+
+# @blueprint.route('/')
+# def index():
+#     return render_template('contact.html')
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('contact.html')
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+
+        # sendEMailToAdmin(contact_form.email.data,contact_form.name.data,contact_form.body.data)
+        flash(
+            f'Thanks {contact_form.name.data}, we have received your message. We will respond soon!', 'success')
+        return redirect(url_for( 'home_blueprint.index'))
+
+    return render_template('contact.html', contact_form=contact_form)
