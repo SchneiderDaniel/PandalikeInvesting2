694ece75efbe800ff7b7df3adf1986313f91755f
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-13 13:37:17 +0100

    tempalte

--- app/base/routes.py
+++ app/base/routes.py
@@ -17,10 +17,6 @@ def route_default():
     return redirect(url_for('home_blueprint.index'))
 
 
-@blueprint.route('/fixed_<template>')
-def route_fixed_template(template):
-    return render_template('fixed/fixed_{}.html'.format(template))
-
 
 @blueprint.route('/page_<error>')
 def route_errors(error):
