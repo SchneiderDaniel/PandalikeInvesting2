5275a9c4f2645139e50ff6a7ff5adb569a58a160
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-13 16:02:01 +0100

    rename

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'home', 'DashExample', 'setting'):
+    for module_name in ('base', 'home', 'tools', 'setting'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
