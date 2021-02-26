3b5896d4bac84d3ec7713b9ca9e28f7f7b4abf7a
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-26 20:01:46 +0100

    Template for Dash

--- Dashapps/Dash_App1.py
+++ Dashapps/Dash_App1.py
@@ -6,49 +6,62 @@ from .Dash_fun import apply_layout_with_auth, load_object, save_object
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+from .dash_base import warning_card, colors
 
 url_base = '/dash/app1/'
 
-import plotly.graph_objs as go
-fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+def description_card():
+    return html.Div(
+        id="description-card",
+        children="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    )
+
+# The Layout
+layout = html.Div(style={'font-family':'"Poppins", sans-serif'}, children=[
+    html.H1(
+        children='This is a title',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Div(children=description_card(), style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    html.Br(),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    ),
+
+    html.Div(children=warning_card(), style={
+        'textAlign': 'left',
+        'color': colors['text']
+    })
 
 
-layout = html.Div([
 
-    #First row
-    html.Div([
-        #Description
-        html.Div([
-            html.H3('Description'),
-            dcc.Graph(id='g1', figure={'data': [{'y': [1, 2, 3]}]})
-        ], className="six columns"),
+])
 
-        #Parameters
-        html.Div([
-            html.H3('Parameters'),
-            dcc.Graph(id='g2', figure={'data': [{'y': [1, 2, 3]}]})
-        ], className="six columns"),
-    ], className="row"),
-
-    #Result
-        html.Div([
-            html.H3('Result'),
-            dcc.Graph(id='g3', figure={'data': [{'y': [1, 2, 3]}]})
-        ], className="twelve columns"),
-
-
-], style={'font-family':'"Poppins", sans-serif'})
-
-# layout = html.Div(className='row', children=[
-#     html.H1("Tips database analysis (First dashboard)"),
-#     dcc.Dropdown(),
-#     html.Div(children=[
-#         dcc.Graph(id="graph1", style={'display': 'inline-block'}),
-#         dcc.Graph(id="graph2", style={'display': 'inline-block'})
-#     ])
-# ])
-
-#font-family: "Helvetica Neue", Roboto, Arial, "Droid Sans", sans-serif;
 
 def Add_Dash(server):
     app = Dash(server=server, url_base_pathname=url_base)
