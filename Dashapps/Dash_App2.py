3745f5d91ac22393e7d9898c679fa5acacfd1a10
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-27 19:47:39 +0100

    First demo version rdy

--- Dashapps/Dash_App2.py
+++ Dashapps/Dash_App2.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from dash import Dash
-from dash.dependencies import Input, State, Output, ClientsideFunction
+from dash.dependencies import Input, Output, ALL, State, MATCH, ALLSMALLER, ClientsideFunction
 from .Dash_fun import apply_layout_with_auth,apply_layout_without_auth, load_object, save_object
 import dash_core_components as dcc
 import dash_html_components as html
+
 
 import dash
 import dash_table
@@ -19,35 +20,7 @@ url_base = '/dash/app2/'
 
 df2 = pd.read_csv('./app/base/static/testdata/solar.csv')
 
-list_id_tickier =  []
-list_id_percent = []
 
-def step():
-    id_ticker = str(np.random.randn())
-    id_percent = str(np.random.randn())
-    return html.Div(
-        children=[
-            dbc.Row(
-                [
-                    dbc.Col( 
-                        children=[
-                            html.Div("Ticker:"),
-                            dbc.Input(type="text", placeholder="Enter a ticker",id=id_ticker)
-                        ],
-                        width=6
-                    ),
-                    dbc.Col( 
-                        children=[
-                            html.Div("Percent:"),
-                            dbc.Input(type="number", placeholder="Enter %",id=id_percent)
-                        ],
-                        width=4
-                    )
-                ],
-                style = { 'width': '80%'}
-            ),
-            html.Br(),
-    ])
 
 def description_card():
     return html.Div(
@@ -62,9 +35,8 @@ def ticker_card():
     return html.Div(
         children=[
             html.H3(children='Portfolio'),
-            html.Div(children=[step()], id='step_list'),
-            dbc.Button('Add Ticker', color="secondary", id='add_step_button', n_clicks_timestamp='0', className="mr-1"),
-            dbc.Button('Remove Ticker', color="secondary", id='remove_step_button', n_clicks_timestamp='0', className="mr-1")
+            html.Div(children=[], id='container_ticker'),
+            dbc.Button('Add Ticker', color="secondary", id='add_ticker_button',  n_clicks=1, className="mr-1"),
             ],
         style={
         'backgroundColor': colors['background'],
@@ -132,26 +104,64 @@ def Add_Dash(server):
     apply_layout_without_auth(app, layout)
 
     @app.callback(
-        dash.dependencies.Output('step_list', 'children'),
-        [dash.dependencies.Input('add_step_button', 'n_clicks_timestamp'),
-        dash.dependencies.Input('remove_step_button', 'n_clicks_timestamp')],
-        [dash.dependencies.State('step_list', 'children')])
-    def add_step(add_ts, remove_ts, div_list):
-        add_ts = int(add_ts)
-        remove_ts = int(remove_ts)
-        if add_ts > 0 and add_ts > remove_ts:
-            div_list += [step()]
-        if len(div_list) > 1 and remove_ts > add_ts:
-            div_list = div_list[:-1]
-        return div_list
+        Output("compute-output", "children"),
+        [Input(component_id={'type': 'dynamic-ticker', 'index': ALL}, component_property='value'),
+        Input(component_id={'type': 'dynamic-percent', 'index': ALL}, component_property='value')]
+    )
+    def compute(ticker_values, percent_values):
+        print(ticker_values)
+        print(percent_values)
+        # return "Testi"
 
 
+    
     @app.callback(
-    Output("compute-output", "children"), [Input("compute-button", "n_clicks")])
-    def on_button_click(n):
-        if n is None:
-            return "Not clicked."
-        else:
-            return f"Clicked {n} times."
+        Output('container_ticker', 'children'),
+        [Input('add_ticker_button', 'n_clicks')],
+        [State('container_ticker', 'children')]
+    )
+    def display_tickers(n_clicks, div_children):
+
+        new_child= html.Div(
+            children=[
+                dbc.Row(
+                    [
+                        dbc.Col( 
+                            children=[
+                                html.Div("Ticker:"),
+                                dbc.Input(type="text", value='MSFT', placeholder="Enter a ticker",
+                                id={
+                                    'type': 'dynamic-ticker',
+                                    'index': n_clicks
+                                })
+                            ],
+                            width=6
+                        ),
+                        dbc.Col( 
+                            children=[
+                                html.Div("Percent:"),
+                                dbc.Input(type="number", value='5', placeholder="Enter %",
+                                id={
+                                    'type': 'dynamic-percent',
+                                    'index': n_clicks
+                                })
+                            ],
+                            width=4
+                        )
+                    ],
+                    style = { 'width': '80%'}
+                ),
+                html.Br(),
+        ])
+        div_children.append(new_child)
+        return div_children
+
+ 
+
+
+
+
+
+
 
     return app.server
