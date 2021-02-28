830845d30589dbd1a342c69b66dccc2d75fd48c4
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-02-28 19:11:46 +0100

    styling

--- app/tools/routes.py
+++ app/tools/routes.py
@@ -5,12 +5,12 @@ from flask_security import login_required, roles_accepted
 
 
 @blueprint.route('/app1')
-@login_required
+# @login_required
 def app1_template():
     return render_template('app1.html', dash_url = Dash_App1.url_base)
 
 @blueprint.route('/app2')
-@login_required
-@roles_accepted('admin')
+# @login_required
+# @roles_accepted('admin')
 def app2_template():
     return render_template('app2.html', dash_url = Dash_App2.url_base)
