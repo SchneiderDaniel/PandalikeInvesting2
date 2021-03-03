a3f20f8eceed45ec56448a33732509f316f84959
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-03-03 19:48:31 +0100

    drop db

--- flask/app/database.py
+++ flask/app/database.py
@@ -15,3 +15,12 @@ def init_db():
     # you will have to import them first before calling init_db()
     from . import models
     Base.metadata.create_all(bind=engine)
+
+
+def drop_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    from . import models
+    Base.metadata.create_all(bind=engine)
+    Base.metadata.drop_all(bind=engine)
