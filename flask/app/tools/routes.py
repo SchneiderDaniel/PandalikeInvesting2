db0e265b1bdfab462a9b6a7f9346615988365014
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-03-19 17:02:28 +0100

    reroutes

--- flask/app/tools/routes.py
+++ flask/app/tools/routes.py
@@ -4,18 +4,18 @@ from Dashapps import Dash_App1, Dash_App2, Dash_App3
 from flask_security import login_required, roles_accepted
 
 
-@blueprint.route('/app1')
+@blueprint.route('/rebalancing')
 # @login_required
 def app1_template():
     return render_template('app1.html', dash_url = Dash_App1.url_base)
 
-@blueprint.route('/app2')
+@blueprint.route('/correlation')
 # @login_required
 # @roles_accepted('admin')
 def app2_template():
     return render_template('app2.html', dash_url = Dash_App2.url_base)
 
 
-@blueprint.route('/app3')
+@blueprint.route('/impactoffondcosts')
 def app3_template():
     return render_template('app3.html', dash_url = Dash_App3.url_base)
